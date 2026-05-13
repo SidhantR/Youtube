8e6eb5c09d80d201c88c9d8eece0b286a169ba14
Sidhant singh <81099883+SidhantR@users.noreply.github.com>
2026-05-13 23:48:40 +0530

    enable transformer, add auth to trpc context ,  add protected procedure

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,29 +1,47 @@
-import { initTRPC } from '@trpc/server';
- 
-/**
- * This context creator accepts `headers` so it can be reused in both
- * the RSC server caller (where you pass `next/headers`) and the
- * API route handler (where you pass the request headers).
- */
-export const createTRPCContext = async (opts: { headers: Headers }) => {
-  // const user = await auth(opts.headers);
-  return { userId: 'user_123' };
+import { initTRPC, TRPCError } from '@trpc/server';
+import superjson from 'superjson'
+import { auth } from '@clerk/nextjs/server';
+import {db} from '@/db'
+import { users } from '@/db/schema';
+import {eq} from 'drizzle-orm'
+
+
+export async function createTRPCContext () {
+  // lightweight request info
+  // runs for every single request/procedure call - thats why not doing DB call here
+  const {userId} = await auth();
+
+  return { clerkUserId: userId }
 };
- 
-// Avoid exporting the entire t-object
-// since it's not very descriptive.
-// For instance, the use of a t variable
-// is common in i18n libraries.
-const t = initTRPC
-  .context<Awaited<ReturnType<typeof createTRPCContext>>>()
+
+export type Context = Awaited<ReturnType<typeof createTRPCContext>>
+
+const t = initTRPC.context<Context>()
   .create({
-    /**
-     * @see https://trpc.io/docs/server/data-transformers
-     */
-    // transformer: superjson,
+    transformer: superjson,
   });
  
 // Base router and procedure helpers
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+
+export const protectedProcedure = t.procedure.use(async function isAuthed(opts) {
+  const {ctx} = opts;
+  if(!ctx.clerkUserId) {
+    throw new TRPCError({ code: "UNAUTHORIZED"})
+  }
+
+  //get user from DB - drizzle
+  const [user] = await db.select().from(users).where(eq(users.clerkId, ctx.clerkUserId))
+  if(!user) {
+    throw new TRPCError({code: "UNAUTHORIZED"})
+  }
+
+  return opts.next({
+    ctx: {
+      ...ctx,
+      user
+    }
+  })
+})
